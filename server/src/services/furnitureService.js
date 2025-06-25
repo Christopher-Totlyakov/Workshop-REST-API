7e69f92c7ecfe5a90f27fa12cd4f92750983d165
Christopher Totlyakov <laminex0622@gmail.com>
2025-06-26 00:10:32 +0300

    add furniture catalog logic

--- server/src/services/furnitureService.js
+++ server/src/services/furnitureService.js
@@ -3,5 +3,8 @@ import Furniture from "../models/furniture.js"
 export default {
     async create(furnitureData){
         return await Furniture.create(furnitureData);
-    }
+    },
+    async getAll(){
+        return await Furniture.find();
+    },
 }
